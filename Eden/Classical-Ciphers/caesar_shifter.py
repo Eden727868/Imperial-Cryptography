968d142c155a2fb9222aea59a0b7496acdfaaf61
Eden727868 <24024692@iclms.ac.uk>
2025-01-21 21:58:01 +0000

    terminology changes, fixed bug where text input was not used as function input variable

--- Eden/Classical-Ciphers/caesar_shifter.py
+++ Eden/Classical-Ciphers/caesar_shifter.py
@@ -3,34 +3,34 @@ from sys import argv
 
 alphabet = "abcdefghijklmnopqrstuvwxyz"
 
-def caesar_shift(plaintext: str, keyset: str, key: int) -> str:
-    ''' Takes 3 paramters: plaintext, keyset (eg alphabet), key
+def caesar_shift(plaintext: str, alphabet: str, key: int) -> str:
+    ''' Takes 3 paramters: plaintext, alphabet, key
         Returns plaintext shifted by the key
     '''
     ciphertext = ""
-    number_of_keys = len(keyset)
+    no_of_chars = len(alphabet)
 
     for char in plaintext:
-        if char.lower() in keyset:
+        if char.lower() in alphabet:
             char = char.lower()
-            index = keyset.index(char)
-            new_index = (index + key) % number_of_keys
-            char = keyset[new_index]
+            index = alphabet.index(char)
+            new_index = (index + key) % no_of_chars
+            char = alphabet[new_index]
             ciphertext += char
         else:
             ciphertext += char
     return ciphertext
 
 
-def caesar_unshift(ciphertext: str, keyset: str, key: int) -> str:
-    ''' Takes 3 parameters: ciphertext, keyset, key
+def caesar_unshift(ciphertext: str, alphabet: str, key: int) -> str:
+    ''' Takes 3 parameters: ciphertext, alphabet, key
         Returns the plaintext shifted in reverse by the key
     '''
-    return caesar_shift(ciphertext, keyset, len(keyset) - key)
+    return caesar_shift(ciphertext, alphabet, len(alphabet) - key)
 
 
-def brute_force(ciphertext: str, keyset: str) -> None:
-    ''' Takes 2 parameters, ciphertext, keyset
+def brute_force(ciphertext: str, alphabet: str) -> None:
+    ''' Takes 2 parameters, ciphertext, alphabet
         Tries every possible key and prints the most likely shift
     '''
     explicit = input("Explicit output? [y/n]\n")
@@ -38,8 +38,8 @@ def brute_force(ciphertext: str, keyset: str) -> None:
 
     full_list = dict()
 
-    for current_key in range(0, len(keyset)):
-        current_shift = caesar_shift(ciphertext, keyset, current_key)
+    for current_key in range(0, len(alphabet)):
+        current_shift = caesar_shift(ciphertext, alphabet, current_key)
         full_list[f"{current_key}: {current_shift}"] = get_englishness(current_shift, speed)
 
         if explicit == "y":
@@ -51,17 +51,16 @@ def brute_force(ciphertext: str, keyset: str) -> None:
 if __name__ == "__main__":
     if len(argv) != 2:
         print("Please enter 1 command-line argument: 0 to encode, 1 to decode, 2 to brute force")
-        quit()
+    else:
+        text_input = input("Enter string\n")
+        
+        if argv[1] == "0":
+            key = int(input("Enter key [1-26]: "))
+            print(caesar_shift(text_input, alphabet, key))
 
-    text_input = input("Enter string\n")
-    
-    if argv[1] == "0":
-        key = int(input("Enter key [1-26]: "))
-        print(caesar_shift(string, alphabet, key))
+        elif argv[1] == "1":
+            key = int(input("Enter key [1-26]: "))
+            print(caesar_unshift(text_input, alphabet, key))
 
-    elif argv[1] == "1":
-        key = int(input("Enter key [1-26]: "))
-        print(caesar_unshift(string, alphabet, key))
-
-    elif argv[1] == "2":
-        brute_force(text_input, alphabet)
+        elif argv[1] == "2":
+            brute_force(text_input, alphabet)
